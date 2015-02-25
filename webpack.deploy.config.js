1a23d7262c736e31e638135cff41e95d9e278b77
trashgenerator <trashgenerator@gmail.com>
2015-02-25 16:12:52 +0300

    webpack

--- webpack.deploy.config.js
+++ webpack.deploy.config.js
@@ -8,6 +8,19 @@ config["output"] = {
     filename: 'bundle.js',
     publicPath: '/static/build/bundle.js'
 };
+
+config["module"] = {
+    loaders: [
+        // Pass *.jsx files through jsx-loader transform
+        {test: /\.jsx$/, loaders: ['jsx']},
+        {test: /\.css$/, loader: "style!css"},
+        {test: /\.png$/, loader: "file"},
+        {test: /\.jpg$/, loader: "file"},
+        {test: /\.(ttf|eot|svg|woff|ttf)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"}
+        //{	test: "png|jpg|jpeg|gif|svg", loader: "url-loader?limit=10000",}
+
+    ]
+};
 config["plugins"] = [];
 
 module.exports = config;
