8c8d9b32af2931dcf4ac8b96d2d35fa821f1c569
trashgenerator <trashgenerator@gmail.com>
2015-02-26 11:39:09 +0300

    minify

--- webpack.deploy.config.js
+++ webpack.deploy.config.js
@@ -1,5 +1,7 @@
 var config = require("./webpack.config");
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var webpack = require('webpack');
+
 
 config["entry"] = [
     './entry.jsx',
@@ -15,10 +17,10 @@ config["module"] = {
     loaders: [
         // Pass *.jsx files through jsx-loader transform
         {test: /\.jsx$/, loaders: ['jsx']},
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract("style-loader", "css-loader")
-            },
+        {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+        },
         {test: /\.png$/, loader: "file"},
         {test: /\.jpg$/, loader: "file"},
         {test: /\.(ttf|eot|svg|woff|ttf)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader"}
@@ -27,7 +29,8 @@ config["module"] = {
     ]
 };
 config["plugins"] = [
-        new ExtractTextPlugin("[name].css")
+    new ExtractTextPlugin("[name].css"),
+    new webpack.optimize.UglifyJsPlugin({minimize: true})
 ];
 
 module.exports = config;
