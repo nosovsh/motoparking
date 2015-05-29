1bb6a51848bc393324942e6671b5ae708c31ea28
trashgenerator <trashgenerator@gmail.com>
2015-05-29 11:59:26 +0300

    not authorized file upload

--- components/FileUploader/index.jsx
+++ components/FileUploader/index.jsx
@@ -16,7 +16,7 @@ $.cloudinary.config({cloud_name: 'motoparking', api_key: '666287416361873'});
 
 var FileUploader = React.createClass({
 
-    mixins: [FluxMixin, StoreWatchMixin("ParkingStore", "ParkingImageStore")],
+    mixins: [FluxMixin, StoreWatchMixin("ParkingStore", "CurrentUserStore")],
 
     propTypes: {},
 
@@ -39,87 +39,93 @@ var FileUploader = React.createClass({
             progressBarStyle["display"] = "none";
         return (
             <div className="FileUploader">
-                <label className="FileUploader__label" style={ addImageStyle }>
+                <label className="FileUploader__label" style={ addImageStyle } onClick={ this.onFileUploaderClick }>
                     <Icon name="add-image" />
-                    <input type="file" ref="fileInput" name="file"/>
+                    { this.state.isAuthorized ? <input type="file" ref="fileInput" name="file"/> : null }
                 </label>
-                    <div className="Progressbar" ref="progressbar" style={progressBarStyle}></div>
-                    <div className="FileUploader__CancelWrapper" style={progressBarStyle}>
-                        <Icon name="close" onClick={ this.onCancelUpload } />
-                    </div>
+                <div className="Progressbar" ref="progressbar" style={progressBarStyle}></div>
+                <div className="FileUploader__CancelWrapper" style={progressBarStyle}>
+                    <Icon name="close" onClick={ this.onCancelUpload } />
+                </div>
             </div>
         )
     },
-
+    onFileUploaderClick: function () {
+        if (!this.state.isAuthorized) {
+            this.getFlux().actions.authorizationRequired()
+        }
+    },
     componentDidMount: function () {
-        console.log("componentDidMount")
-        // TODO: remove global object!
-        var options = {};
+        if (this.state.isAuthorized) {
+            // TODO: remove global object!
+            var options = {};
 
-        if (window.fileUploadCounter == undefined)
-            window.fileUploadCounter = 0;
+            if (window.fileUploadCounter == undefined)
+                window.fileUploadCounter = 0;
 
-        if (window.fileUploadCounter != 0)
-            options.dropZone = null;
+            if (window.fileUploadCounter != 0)
+                options.dropZone = null;
 
-        window.fileUploadCounter++;
-
-
-        var fileInput = this.refs.fileInput.getDOMNode();
-        $(fileInput).unsigned_cloudinary_upload("zpwu8wz2",
-            {cloud_name: 'motoparking', tags: 'browser_uploads'},
-            options
-        ).bind('fileuploadstart', function (e, data) {
-                console.log("fileuploadstart");
-                this.setState({
-                    uploading: true
-                });
-            }.bind(this)
-        ).bind('cloudinarydone', function (e, data) {
-                console.log("cloudinarydone");
-                this.setState({
-                    uploading: false
-                });
-                this.getFlux().actions.postParkingImage({
-                    "cloudinaryId": data.result.public_id,
-                    "parking": this.state.currentParkingId
-                });
-            }.bind(this)
-        ).bind('cloudinaryprogress', function (e, data) {
-                var loaded = data.loaded / data.total;
-                console.log(loaded);
-                console.log(data);
-                //this.getFlux().actions.showParkingImageUploadProgress(loaded);
-                this.circle.animate(loaded)
-            }.bind(this)
-        ).bind('fileuploadadd', function (e, data) {
-                this.jqXHR = data.submit(); // Catching the upload process of every file
-            }.bind(this));
+            window.fileUploadCounter++;
 
 
-        var circleNode = this.refs.progressbar.getDOMNode();
+            var fileInput = this.refs.fileInput.getDOMNode();
+            $(fileInput).unsigned_cloudinary_upload("zpwu8wz2",
+                {cloud_name: 'motoparking', tags: 'browser_uploads'},
+                options
+            ).bind('fileuploadstart', function (e, data) {
+                    console.log("fileuploadstart");
+                    this.setState({
+                        uploading: true
+                    });
+                }.bind(this)
+            ).bind('cloudinarydone', function (e, data) {
+                    console.log("cloudinarydone");
+                    this.setState({
+                        uploading: false
+                    });
+                    this.getFlux().actions.postParkingImage({
+                        "cloudinaryId": data.result.public_id,
+                        "parking": this.state.currentParkingId
+                    });
+                }.bind(this)
+            ).bind('cloudinaryprogress', function (e, data) {
+                    var loaded = data.loaded / data.total;
+                    console.log(loaded);
+                    console.log(data);
+                    //this.getFlux().actions.showParkingImageUploadProgress(loaded);
+                    this.circle.animate(loaded)
+                }.bind(this)
+            ).bind('fileuploadadd', function (e, data) {
+                    this.jqXHR = data.submit(); // Catching the upload process of every file
+                }.bind(this));
 
-        this.circle = new ProgressBar.Circle(circleNode, {
-            color: '#979797',
-            strokeWidth: 1
-        });
+
+            var circleNode = this.refs.progressbar.getDOMNode();
+
+            this.circle = new ProgressBar.Circle(circleNode, {
+                color: '#979797',
+                strokeWidth: 1
+            });
+        }
     },
 
     componentWillUnmount: function () {
-        var fileInput = this.refs.fileInput.getDOMNode();
-
-        $(fileInput).fileupload('destroy');
-        window.fileUploadCounter--;
-
+        if (this.state.isAuthorized) {
+            var fileInput = this.refs.fileInput.getDOMNode();
+            $(fileInput).fileupload('destroy');
+            window.fileUploadCounter--;
+        }
     },
 
     getStateFromFlux: function () {
         var store = this.getFlux().store("ParkingStore");
-        var parkingImageStore = this.getFlux().store("ParkingImageStore");
+        var currentUserStore = this.getFlux().store("CurrentUserStore");
 
         return {
             error: store.error,
-            currentParkingId: store.currentParkingId
+            currentParkingId: store.currentParkingId,
+            isAuthorized: currentUserStore.isAuthorized()
         };
     },
 
