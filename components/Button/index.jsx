0a58a8d02fbb95067d9317e7003e7e853b944dea
trashgenerator <trashgenerator@gmail.com>
2015-09-24 23:37:17 +0300

    Button and ButtonRow refactoring

--- components/Button/index.jsx
+++ components/Button/index.jsx
@@ -1,23 +1,24 @@
 var React = require("react/addons");
+var classNames = require("classnames");
 
 require("./style.css");
 
 
 var Button = React.createClass({
-    propTypes: {
-        text: React.PropTypes.string.isRequired,
-        callback: React.PropTypes.func.isRequired,
-        selected: React.PropTypes.bool
-    },
-    render: function () {
-        var classes = {
-            button: true,
-            button__selected: this.props.selected
-        }
-        return (
-            <div className={ React.addons.classSet(classes) } onClick={ this.props.callback }>{ this.props.text }</div>
-        )
-    }
+  propTypes: {
+    text: React.PropTypes.string.isRequired,
+    callback: React.PropTypes.func.isRequired,
+    selected: React.PropTypes.bool
+  },
+  render: function() {
+    return (
+      <div
+        className={ classNames("button", this.props.selected && "button__selected") }
+        onClick={ this.props.callback }>
+          { this.props.text }
+      </div>
+    );
+  }
 });
 
 module.exports = Button;
--- components/ButtonRow/index.jsx
+++ components/ButtonRow/index.jsx
@@ -1,38 +1,42 @@
 var React = require("react/addons");
+var classNames = require("classnames");
 
 require("./style.css");
 
 
 var ButtonRow = React.createClass({
-    propTypes: {
-        selected: React.PropTypes.bool,
-        bordered: React.PropTypes.bool,
-        color: React.PropTypes.string,
-        height: React.PropTypes.string,
-        align: React.PropTypes.string
-    },
-    getDefaultProps: function () {
-        return {
-            selected: false,
-            bordered: false
-        };
-    },
-    render: function () {
-        var classes = {
-            ButtonRow: true,
-            ButtonRow__selected: this.props.selected,
-            ButtonRow_bordered: this.props.bordered
-        };
-        if (this.props.color)
-            classes["ButtonRow_color_" + this.props.color] = true;
-        if (this.props.height)
-            classes["ButtonRow_height_" + this.props.height] = true;
-        if (this.props.align)
-            classes["ButtonRow_align_" + this.props.align] = true;
-        return (
-            <div className={ React.addons.classSet(classes) } onClick={ this.props.callback }>{ this.props.children }</div>
-        )
-    }
+  propTypes: {
+    callback: React.PropTypes.func.isRequired,
+    selected: React.PropTypes.bool,
+    bordered: React.PropTypes.bool,
+    color: React.PropTypes.string,
+    height: React.PropTypes.string,
+    align: React.PropTypes.string,
+    children: React.PropTypes.node
+  },
+  getDefaultProps: function() {
+    return {
+      selected: false,
+      bordered: false
+    };
+  },
+  render: function() {
+    var classes = [
+      "ButtonRow",
+      this.props.selected && "ButtonRow__selected",
+      this.props.bordered && "ButtonRow_bordered",
+      this.props.color && "ButtonRow_color_" + this.props.color,
+      this.props.height && "ButtonRow_height_" + this.props.height,
+      this.props.align && "ButtonRow_align_" + this.props.align
+    ];
+    return (
+      <div
+        className={ classNames(classes) }
+        onClick={ this.props.callback }>
+          { this.props.children }
+      </div>
+    );
+  }
 });
 
 module.exports = ButtonRow;
