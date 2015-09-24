4778a85ccbd0581e553cb98e37b1111aa0747be1
trashgenerator <trashgenerator@gmail.com>
2015-09-25 00:20:33 +0300

    DropDownMenu refactoring

--- components/DropDownMenu/index.jsx
+++ components/DropDownMenu/index.jsx
@@ -1,46 +1,56 @@
-var React = require("react/addons"),
-    _ = require("lodash"),
-    Events = require("utils/events");
+var React = require("react/addons");
+var Events = require("utils/events");
 
 require("./style.css");
 
+
 var DropDownMenu = React.createClass({
+  propTypes: {
+    children: React.PropTypes.node
+  },
 
-    getInitialState: function () {
-        return {
-            open: false
-        }
-    },
-    render: function () {
-        return (
-            <div>
-                { this.state.open ? <div className="DropDownMenu">
-                    <div className="DropDownMenu_Inner">
-                        { this.props.children }
-                    </div>
-                    <div className="PopoverArrow"/>
-                </div> : null}
-            </div>
-        )
-    },
-    componentDidMount: function () {
-        Events.on(document, 'click', this.componentClickAway);
-    },
-    componentWillUnmount: function () {
-        Events.off(document, 'click', this.componentClickAway);
-    },
+  getInitialState: function() {
+    return {
+      open: false
+    };
+  },
 
-    componentClickAway: function () {
-        if (this.state.open) {
-            this.setState({open: false});
-        }
-    },
-    toggle: function () {
-        setTimeout(function () {
-            this.setState({open: !this.state.open});
-        }.bind(this), 0);
-        return this;
+  componentDidMount: function() {
+    Events.on(document, "click", this.componentClickAway);
+  },
+
+  componentWillUnmount: function() {
+    Events.off(document, "click", this.componentClickAway);
+  },
+
+  componentClickAway: function() {
+    if (this.state.open) {
+      this.setState({open: false});
     }
+  },
+
+  toggle: function() {
+    setTimeout(function() {
+      this.setState({open: !this.state.open});
+    }.bind(this), 0);
+    return this;
+  },
+
+  render: function() {
+    return (
+      <div>
+        {
+          this.state.open ?
+          <div className="DropDownMenu">
+            <div className="DropDownMenu_Inner">
+              { this.props.children }
+            </div>
+            <div className="PopoverArrow"/>
+          </div> : null
+          }
+      </div>
+    );
+  }
 });
 
 module.exports.DropDownMenu = DropDownMenu;
