8a906f0e19eebc986a510b8dc778847204724f6c
trashgenerator <trashgenerator@gmail.com>
2015-09-24 23:18:33 +0300

    Modal refactoring

--- components/Modal/index.jsx
+++ components/Modal/index.jsx
@@ -1,62 +1,55 @@
-var React = require("react"),
-    Fluxxor = require("fluxxor");
+var React = require("react");
 
-require("./style.css");
-
-var FluxMixin = Fluxxor.FluxMixin(React),
-    StoreWatchMixin = Fluxxor.StoreWatchMixin;
-
-var Router = require('react-router'),
-    Link = Router.Link;
+var Router = require("react-router");
+var Link = Router.Link;
 
 var Icon = require("../Icon");
 
+require("./style.css");
+
 
 var Modal = React.createClass({
-    mixins: [FluxMixin],
-    render: function () {
-        return (
+  render: function() {
+    return (
+      <div className="Modal" >
+        <div className="Modal__Content">
+          <div className="Modal__Content__Inner">
+            <Link to="Default" style={ {color: "#FFF"} } >
+              <div className="close-wrapper">
+                <Icon name="close" />
+              </div>
+            </Link>
+            <div>
+              <img src="http://res.cloudinary.com/motoparking/image/upload/c_scale,w_1000/v1431071592/i2hne4u3ctv9ffoqi49i.jpg" className="Modal__Cover"/>
+              <div className="Modal__Text__Wrapper">
+                <h2 className="Modal__Header">Сервис поиска парковок для мотоциклов</h2>
+                <div className="Modal__Text">
 
-            <div className="Modal" >
-                <div className="Modal__Content">
-                    <div className="Modal__Content__Inner">
-                        <Link to="Default" style={ {color: "#FFF"} } >
-                            <div className="close-wrapper">
-                                <Icon name="close" />
-                            </div>
-                        </Link>
-                        <div>
-                            <img src="http://res.cloudinary.com/motoparking/image/upload/c_scale,w_1000/v1431071592/i2hne4u3ctv9ffoqi49i.jpg" className="Modal__Cover"/>
-                            <div className="Modal__Text__Wrapper">
-                                <h2 className="Modal__Header">Сервис поиска парковок для мотоциклов</h2>
-                                <div className="Modal__Text">
+                  <p>Охраняемые парковки – практически единственный действенный способ защитить мотоцикл от угона.</p>
+                  <p>Этот сайт – попытка создать единую базу парковок на которые пускают мотоциклы.</p>
+                  <p>Прмиерный план действий:</p>
+                  <ol className="NoBullets">
+                    <li>Находишь парковку в нужном районе</li>
+                    <li>Изучаешь комментарии и фотки, оценивешь на сколько она безопасна</li>
+                    <li>Помогаешь мото братьям – добавляешь информацию о парковках, которых еще нет на сайте</li>
+                    <li>…</li>
+                    <li>Profit</li>
+                  </ol>
+                  <p>
+                    Напишите нам:
+                    <a href="mailto:info@motoparking.club">info@motoparking.club</a>
+                  </p>
+                  <p>До встречи на дороге!</p>
 
-                                    <p>Охраняемые парковки – практически единственный действенный способ защитить мотоцикл от угона.</p>
-                                    <p>Этот сайт – попытка создать единую базу парковок на которые пускают мотоциклы.</p>
-                                    <p>Прмиерный план действий:</p>
-                                    <ol className="NoBullets">
-                                        <li>Находишь парковку в нужном районе</li>
-                                        <li>Изучаешь комментарии и фотки, оценивешь на сколько она безопасна</li>
-                                        <li>Помогаешь мото братьям – добавляешь информацию о парковках, которых еще нет на сайте</li>
-                                        <li>…</li>
-                                        <li>Profit</li>
-                                    </ol>
-                                    <p>
-                                        Напишите нам: <a href="mailto:info@motoparking.club">info@motoparking.club</a>
-                                    </p>
-                                    <p>До встречи на дороге!</p>
-
-                                </div>
-                            </div>
-
-
-                        </div>
-                    </div>
                 </div>
-                <div className="Backdrop"></div>
+              </div>
             </div>
-        );
-    }
+          </div>
+        </div>
+        <div className="Backdrop"></div>
+      </div>
+    );
+  }
 });
 
 module.exports = Modal;
