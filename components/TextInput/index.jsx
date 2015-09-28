b71560c3d9ce1d9831890108ecd99a24aaddd63c
trashgenerator <trashgenerator@gmail.com>
2015-09-28 14:38:22 +0300

    TextInput refactoring

--- components/TextInput/index.jsx
+++ components/TextInput/index.jsx
@@ -4,16 +4,11 @@ require("./style.css");
 
 
 var TextInput = React.createClass({
-    propTypes: {
-    },
-    render: function () {
-        var classes = {
-            TextInput: true
-        };
-        return (
-            <input type="text" className={ React.addons.classSet(classes) } placeholder="Не знаю" { ...this.props }/>
-        )
-    }
+  render: function() {
+    return (
+      <input type="text" className="TextInput" placeholder="Не знаю" { ...this.props }/>
+    );
+  }
 });
 
 module.exports = TextInput;
