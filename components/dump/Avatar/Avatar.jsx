9348d91d3e8ff68e8bc6b057f2ddadf7343c95b9
trashgenerator <trashgenerator@gmail.com>
2015-10-05 22:40:35 +0300

    avatar styling

--- components/dump/Avatar/Avatar.jsx
+++ components/dump/Avatar/Avatar.jsx
@@ -8,19 +8,24 @@ var placeholder = require("./placeholder.svg");
 var Avatar = React.createClass({
   propTypes: {
     user: React.PropTypes.object.isRequired,
-    size: React.PropTypes.oneOf(["small", "big"])
+    size: React.PropTypes.oneOf(["small", "medium", "big"]),
+    border: React.PropTypes.oneOf(["thin"])
   },
 
   getDefaultProps: function() {
     return {
-      size: "big"
+      size: "medium"
     };
   },
 
   render: function() {
     var image = this.props.user.image || placeholder;
     return (
-      <img className={ classNames("Avatar", "Avatar_size_" + this.props.size) } src={ image }/>
+      <img className={ classNames(
+        "Avatar",
+        "Avatar_size_" + this.props.size,
+        this.props.border && "Avatar_border_" + this.props.border
+      ) } src={ image }/>
     );
   }
 });
